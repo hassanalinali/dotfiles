4df85700a46e1a967d8caf75ee1b6e6333c99ac8
Alin Ali Hassan <hassanalinali@gmail.com>
2018-10-14 17:33:57 +0200

    Added Wallpaper

--- theme.sh
+++ theme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+dir=~/.dotfiles/
+
 # Interface
 git clone https://github.com/EliverLara/Ant /usr/share/themes/Ant
 
@@ -12,3 +14,7 @@ sudo apt-get update
 sudo apt-get install papirus-icon-theme
 
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
+
+# Wallpaper
+
+gsettings set org.gnome.desktop.background picture-uri "file://${dir}/theme/wallpaper.png"
